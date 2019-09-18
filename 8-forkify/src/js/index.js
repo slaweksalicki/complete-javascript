1496ed8a581920f99c3575416897a7c0ebf798aa
Slawomir Salicki <slawomir.salicki@tangent.co.uk>
2019-09-18 17:56:21 +0100

    Forkify after conf

--- 8-forkify/src/js/index.js
+++ 8-forkify/src/js/index.js
@@ -1,2 +1,4 @@
-// Global app controller
-
+import str from './models/Search';
+import { add, multiply, ID } from './views/searchView';
+console.log(`Hey there!!!`)
+console.log(`Using imported functions ${add(ID, 2)} and ${multiply(3, 5)}, ${str}`);
