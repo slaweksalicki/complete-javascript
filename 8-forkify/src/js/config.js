8d1f08d9a5433dd6f242a19da6fd7c8cc90cb9ff
Slawomir Salicki <slawomir.salicki@tangent.co.uk>
2019-11-14 15:23:40 +0000

    Finished project commit

---
+++ 8-forkify/src/js/config.js
@@ -0,0 +1,1 @@
+export const key = '214207fc7acbbfaa3d17e92ba2442479';
--- 8-forkify/src/js/models/Search.js
+++ 8-forkify/src/js/models/Search.js
@@ -1,1 +1,17 @@
-export default 'I am an exported string';
+import axios from 'axios';
+import { key } from '../config';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults() {
+    try {
+      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      //console.log(this.result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
